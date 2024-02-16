b316c7d6c32e37476bf960adc0177f190fff62a6
Yukun SHANG <ykshang4119@gmail.com>
2024-02-17 01:45:14 +0800

    add some todos

---
+++ consts/uml.go
@@ -0,0 +1,9 @@
+package consts
+
+const (
+	UmlStartuml      = "@startuml"
+	UmlSetAutonumber = "autonumber"
+	UmlEnduml        = "@enduml"
+
+	RelativeYamlConfigFilePath = "./config/config.yml"
+)
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"go/token"
 	"gopkg.in/yaml.v3"
 	"log"
+	"lunar_uml/consts"
 	"lunar_uml/service"
 	"os"
 )
@@ -23,15 +24,20 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	ast.Print(fset, node)
+
+	err = ast.Print(fset, node)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Get all the participants
 	lunar.Participants = append(lunar.Participants, lunar.Config.LunarConfig.TargetInf)
 
 	// Start PlantUML sequence diagram
-	lunar.PlantUML = append(lunar.PlantUML, "@startuml")
-	lunar.PlantUML = append(lunar.PlantUML, "autonumber")
+	lunar.PlantUML = append(lunar.PlantUML, consts.UmlStartuml)
+	lunar.PlantUML = append(lunar.PlantUML, consts.UmlSetAutonumber)
 
+	// TODO: make the code neater
 	ast.Inspect(node, func(n ast.Node) bool {
 		switch fn := n.(type) {
 		case *ast.FuncDecl:
@@ -43,16 +49,17 @@ func main() {
 	})
 
 	// End PlantUML sequence diagram
-	lunar.PlantUML = append(lunar.PlantUML, "@enduml")
+	lunar.PlantUML = append(lunar.PlantUML, consts.UmlEnduml)
 
 	// Print PlantUML
 	for _, line := range lunar.PlantUML {
+		// TODO: output to a file
 		println(line)
 	}
 }
 
 func loadConfig() service.YamlConfig {
-	file, err := os.ReadFile("./config/config.yml")
+	file, err := os.ReadFile(consts.RelativeYamlConfigFilePath)
 	if err != nil {
 		log.Fatal("read file error")
 	}
