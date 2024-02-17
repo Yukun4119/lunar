5274296e65d0971a42e4651f038b88d13a24ed4a
Yukun SHANG <ykshang4119@gmail.com>
2024-02-17 11:47:54 +0800

    fix bug

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"gopkg.in/yaml.v3"
 	"log"
 	"lunar_uml/consts"
+	"lunar_uml/models"
 	"lunar_uml/service"
 	"os"
 )
@@ -25,12 +26,12 @@ func initLogConfig() {
 	log.Println("Finish init log")
 }
 
-func loadConfig() service.YamlConfig {
+func loadConfig() models.YamlConfig {
 	file, err := os.ReadFile(consts.RelativeYamlConfigFilePath)
 	if err != nil {
 		log.Fatal("read file error")
 	}
-	config := service.YamlConfig{}
+	config := models.YamlConfig{}
 	err = yaml.Unmarshal(file, &config)
 	if err != nil {
 		log.Fatal("unmarshal error")
